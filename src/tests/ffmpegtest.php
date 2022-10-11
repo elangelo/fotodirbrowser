82487f28487c3a0777598b507eeee4c7a04698b6
Samuel Verstraete <samuel.verstraete@gmail.com>
2022-10-11 16:25:24 +0200

    add recoding code in test

--- src/tests/ffmpegtest.php
+++ src/tests/ffmpegtest.php
@@ -4,16 +4,36 @@
 //https://github.com/PHP-FFMpeg/PHP-FFMpeg
 
 require __DIR__ . '/../../vendor/autoload.php';
-use FFMpeg\FFProbe;
 
-$ffmpeg = FFMpeg\FFMpeg::create();
+use FFMpeg\FFMpeg;
+use FFMpeg\FFProbe;
+use FFMpeg\Format;
+use FFMpeg\Coordinate;
+
+$ffmpeg = FFMpeg::create();
 
 $video = $ffmpeg->open( __DIR__ . '/../../docs/example/2020/2020-09-17/VID_20200917_154600.mp4');
-$frame = $video->frame(FFMpeg\Coordinate\TimeCode::fromSeconds(2));
+$frame = $video->frame(Coordinate\TimeCode::fromSeconds(2));
 $frame->save('VID_20200917_154600.jpg');
 
+$format = new Format\Video\X264();
+// $format->setInitialParameters(array(/*'-movflags',*/ '+faststart', '-preset', 'superfast'));
 
-$ffprobe = FFMpeg\FFProbe::create();
+$maxDimension = new Coordinate\Dimension(720,720);
+
+$video = $ffmpeg->open( __DIR__ . '/../../docs/example/2020/2020-09-17/VID_20200917_154600.mp4');
+$video
+ ->filters()
+ ->resize($maxDimension, 'inset')
+ ->synchronize();
+
+ $video
+ ->save($format, 'test.mp4');
+ //  new FFMpeg\Coordinate\Dimension(720,720))
+
+
+
+$ffprobe = FFProbe::create();
 $videostream = $ffprobe
     ->streams(__DIR__ . '/../../docs/example/2020/2020-09-17/VID_20200917_154600.mp4')
     ->videos()
